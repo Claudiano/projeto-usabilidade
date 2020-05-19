cc41379fb8b0ab3c04698a622c29b397834d525c
Claudiano <claudiano@github.com>
2020-05-19 13:11:00 -0300

    Projeto base

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import "./styles.css"
+import { Link } from 'react-router-dom';
+
+
+function Menu(){
+
+    const pages = [
+        {name: "Inicio", path: "/"},
+        {name: "Turmas", path: "/turmas"},
+        {name: "chat", path: "/chat"},
+        {name: "sair", path: "/"},
+    ]
+
+
+    return(
+        <div className="ctn-menu">
+            <div className="cnt-image">
+                <h1> imagem</h1>
+
+            </div>
+            <div className="list-menu">
+                {pages.map( page => (
+                    <div className="menu-item">
+                        <Link to={page.path} >{page.name}</Link>
+                    </div>
+                ))}
+            </div>
+          
+        </div>
+
+    );
+}
+
+export default Menu;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Menu from '../../components/menu/Menu';
+
+
+function Home() {
+    return(
+        <>  
+            <Menu />
+        </>
+    );
+}
+
+export default Home;
